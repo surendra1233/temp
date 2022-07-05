df15ff291d273557aff8837849b18d7a4de89d66
Surendra Kumar <surendra.kumar@research.iiit.ac.in>
2022-07-05 12:37:03 +0530

    added working app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { React, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from 'react-router-dom';
+import Shop from './components/shop/shop';
+import Cart from './components/cart/cart';
+import { cartContext, cartCountContext } from './components/context';
 import './App.css';
+import './font-awesome-4.7.0/css/font-awesome.min.css';
 
 function App() {
+  const cart = useState([]);
+  const cartCount = useState([0]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <cartContext.Provider value={cart}>
+        <cartCountContext.Provider value={cartCount}>
+          <Router>
+            <div className="header">
+              <ul className="navbar">
+                <li>
+                  <Link to="/">SHOP</Link>
+                </li>
+                <li className="cartIcon">
+                  <Link to="/cart">
+                    <i
+                      class="fa fa-cart-arrow-down"
+                      aria-hidden="true"
+                    ></i>
+                    <sup>{cartCount[0]}</sup>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <Routes>
+              <Route exact path="/" element={<Shop />}></Route>
+              <Route exact path="/cart" element={<Cart />}></Route>
+            </Routes>
+          </Router>
+        </cartCountContext.Provider>
+      </cartContext.Provider>
     </div>
   );
 }
